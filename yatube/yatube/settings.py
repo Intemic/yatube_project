81cf8cc0f40e91e7d5e82b48960c841fabec76b1
Антон Лучик <Intemic@yandex.ru>
2023-01-15 13:33:39 +0500

    Добавлены urls и view-функции

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'post.app.PostConfig'
+    'post.apps.PostConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
