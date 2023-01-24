6afda0dfff25aa37fe76501dcffb139205129741
Антон Лучик <intemic@yandex.ru>
2023-01-24 16:19:46 +0500

    Добавление Group

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
+   # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]   
     template = 'posts/index.html'
     title = 'Это главная страница проекта Yatube'
     context = {
-        'title': title
+        'title': title,
+        'posts' : posts,
     }
     return HttpResponse(render(request, template, context))
 
 
 def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+    
     template = 'posts/group_list.html'
     title = 'Здесь будет информация о группах проекта Yatube'
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'title': title,
+        'group': group,
+        'posts': posts,
     }
     return HttpResponse(render(request, template, context))
